f536f260873ceef1faa62c22c9b77b5dfb13e1ec
NimaBavari <nima.bavari@gmail.com>
2020-10-06 18:08:52 +0400

    Corrected string formatting

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -11,17 +11,17 @@ export default class WeatherCard extends Component {
     return (
       <div className="card weather-card">
         <div className="card-body">
-          <h5 className="card-title">${location.name}, ${location.country}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">${current.temperature} &#176;C</h6>
+          <h5 className="card-title">{location.name}, {location.country}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{current.temperature} &#176;C</h6>
           <div className="media">
             <img
-              src={`${current.weather_icons[0]}`}
+              src={current.weather_icons[0]}
               className="align-self-start mr-3"
-              alt={`${current.weather_descriptions[0]}`}
+              alt={current.weather_descriptions[0]}
             />
             <div className="media-body">
-              <h5 className="mt-0">${current.weather_descriptions[0]}</h5>
-              <small>Last refreshed: ${current.observation_time}</small>
+              <h5 className="mt-0">{current.weather_descriptions[0]}</h5>
+              <small>Last refreshed: {current.observation_time}</small>
             </div>
           </div>
         </div>
